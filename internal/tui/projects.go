8903047fdd9c8624b083de45b7c09fd7fed4073b
Milos Zivlak <zivlakmilos@gmail.com>
2023-11-15 21:13:36 +0100

    Add project

--- internal/tui/projects.go
+++ internal/tui/projects.go
@@ -1,15 +1,29 @@
 package tui
 
 import (
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/zivlakmilos/taskmaster/db"
 )
 
+type ProjectsMode int
+
+const (
+	ProjectsModeNormal ProjectsMode = iota
+	ProjectsModeNew
+	ProjectsModeRename
+	ProjectsModeDelete
+)
+
 type ProjectsModel struct {
 	cfg  Config
-	list list.Model
+	mode ProjectsMode
+
+	list  list.Model
+	input textinput.Model
 }
 
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
@@ -22,8 +36,8 @@ type ProjectItem struct {
 func (i ProjectItem) Title() string       { return i.title }
 func (i ProjectItem) Description() string { return i.description }
 func (i ProjectItem) FilterValue() string { return i.title }
-
 func NewProjectsModel(cfg Config) *ProjectsModel {
+
 	items := []list.Item{
 		ProjectItem{title: "All", description: "All tasks"},
 	}
@@ -31,10 +45,27 @@ func NewProjectsModel(cfg Config) *ProjectsModel {
 	list := list.New(items, list.NewDefaultDelegate(), 0, 0)
 
 	list.Title = "Projects"
+	list.AdditionalShortHelpKeys = func() []key.Binding {
+		return []key.Binding{
+			key.NewBinding(
+				key.WithKeys("a"),
+				key.WithHelp("a", "add project"),
+			),
+			key.NewBinding(
+				key.WithKeys("r"),
+				key.WithHelp("r", "rename project"),
+			),
+			key.NewBinding(
+				key.WithKeys("d"),
+				key.WithHelp("d", "delete project"),
+			),
+		}
+	}
 
 	return &ProjectsModel{
-		cfg:  cfg,
-		list: list,
+		cfg:   cfg,
+		list:  list,
+		input: textinput.New(),
 	}
 }
 
@@ -43,6 +74,9 @@ func (m *ProjectsModel) Init() tea.Cmd {
 }
 
 func (m *ProjectsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		h, v := docStyle.GetFrameSize()
@@ -50,13 +84,94 @@ func (m *ProjectsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		break
 	}
 
-	var cmd tea.Cmd
+	switch m.mode {
+	case ProjectsModeNormal:
+		cmd = m.handleUpdateNormal(msg)
+		break
+	case ProjectsModeNew:
+		cmd = m.handleUpdateNew(msg)
+		break
+	case ProjectsModeRename:
+		cmd = m.handleUpdateRename(msg)
+		break
+	case ProjectsModeDelete:
+		cmd = m.handleUpdateDelete(msg)
+		break
+	}
+	cmds = append(cmds, cmd)
+
 	m.list, cmd = m.list.Update(msg)
-	return m, cmd
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
 }
 
 func (m *ProjectsModel) View() string {
-	return m.list.View()
+	res := m.list.View()
+
+	if m.mode == ProjectsModeNew || m.mode == ProjectsModeRename {
+		res += "\n\n"
+		res += m.input.View()
+	}
+
+	if m.mode == ProjectsModeDelete {
+		res += "\n\n"
+	}
+
+	return res
+}
+
+func (m *ProjectsModel) handleUpdateNormal(msg tea.Msg) tea.Cmd {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "a":
+			m.mode = ProjectsModeNew
+			m.input.SetValue("")
+			m.input.Focus()
+			break
+		case "r":
+			m.mode = ProjectsModeRename
+			m.input.SetValue("")
+			m.input.Focus()
+			break
+		case "d":
+			m.mode = ProjectsModeDelete
+			break
+		case "q":
+			return tea.Quit
+		}
+		break
+	}
+
+	return nil
+}
+
+func (m *ProjectsModel) handleUpdateNew(msg tea.Msg) tea.Cmd {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			m.mode = ProjectsModeNormal
+			return nil
+		case "enter":
+			m.mode = ProjectsModeNormal
+			return addProject(m.cfg, m.input.Value())
+		}
+		break
+	}
+
+	var cmd tea.Cmd
+	m.input, cmd = m.input.Update(msg)
+	return cmd
+}
+
+func (m *ProjectsModel) handleUpdateRename(msg tea.Msg) tea.Cmd {
+	return nil
+}
+
+func (m *ProjectsModel) handleUpdateDelete(msg tea.Msg) tea.Cmd {
+	return nil
 }
 
 func readProjects(cfg Config) []list.Item {
@@ -75,4 +190,24 @@ func readProjects(cfg Config) []list.Item {
 	}
 
 	return projectList
+}
+
+func addProject(cfg Config, name string) tea.Cmd {
+	return func() tea.Msg {
+		con, err := openDb(cfg)
+		if err != nil {
+			return err
+		}
+
+		store := db.NewProjectStore(con)
+
+		project := db.NewProject()
+		project.Name = name
+		err = store.Save(project)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
 }
--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -49,7 +49,7 @@ func (m *MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "ctrl+c":
+		case "ctrl+c":
 			return m, tea.Quit
 		}
 		break
